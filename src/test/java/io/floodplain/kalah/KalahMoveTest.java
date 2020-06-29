479e2a838f06e8a96eb3efaf77729e83f912af1e
Frank Lyaruu <frank@dexels.com>
2020-06-29 20:01:30 +0200

    fix test

--- src/test/java/io/floodplain/kalah/KalahMoveTest.java
+++ src/test/java/io/floodplain/kalah/KalahMoveTest.java
@@ -2,15 +2,29 @@ package io.floodplain.kalah;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.CoreMatchers.anything;
 
 @QuarkusTest
 public class KalahMoveTest {
 
+    @BeforeEach
+    public void setup() {
+        given()
+                .when()
+                .header("Content-Type", ContentType.JSON)
+                .header("Accept",ContentType.JSON)
+                .post("/games")
+                .then()
+                .statusCode(201)
+                .body("id", greaterThanOrEqualTo("1"));
+    }
     @Test
     public void testCreateGame() {
         given()
@@ -20,7 +34,7 @@ public class KalahMoveTest {
           .post("/games")
           .then()
           .statusCode(201)
-             .body("id", is("1"));
+             .body("id", greaterThanOrEqualTo("1"));
     }
 
     @Test
